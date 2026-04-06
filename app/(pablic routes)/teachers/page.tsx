7d7004e03521b53f8437ba808ecdc0f8087b922f
pona <ipona3710@gmail.com>
2026-04-06 16:17:47 +0300

    installation firebase

---
+++ app/(pablic routes)/teachers/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./TeachersPage.module.css";
+
+export default function TeachersPage() {
+  return (
+    <div className={styles.section}>
+      <div className={styles.container}></div>
+    </div>
+  );
+}
